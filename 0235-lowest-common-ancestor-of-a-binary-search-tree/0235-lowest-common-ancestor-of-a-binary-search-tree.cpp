c201d1bae47b4a74648f1a695dda3af0f5175b9c
Chinmay Malve <127118425+chinmay2814@users.noreply.github.com>
2024-07-05 19:31:23 +0530

    Time: 27 ms (25.97%), Space: 21.8 MB (21.23%) - LeetHub

---
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -0,0 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        vector<TreeNode*> pa, pb;
+        TreeNode *node = root;
+
+        // Find the path to node p
+        while (node != NULL) {
+            pa.push_back(node);
+            if (node->val > p->val) {
+                node = node->left;
+            } else if (node->val < p->val) {
+                node = node->right;
+            } else {
+                break;
+            }
+        }
+
+        // Find the path to node q
+        node = root;
+        while (node != NULL) {
+            pb.push_back(node);
+            if (node->val > q->val) {
+                node = node->left;
+            } else if (node->val < q->val) {
+                node = node->right;
+            } else {
+                break;
+            }
+        }
+
+        // Find the lowest common ancestor
+        TreeNode *ans = NULL;
+        for (int i = 0; i < min(pa.size(), pb.size()); ++i) {
+            if (pa[i] == pb[i]) {
+                ans = pa[i];
+            } else {
+                break;
+            }
+        }
+
+        return ans;
+    }
+};
