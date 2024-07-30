c41e3af24ab8038ec9bce972d4680ba55c5cbc12
Chinmay Malve <127118425+chinmay2814@users.noreply.github.com>
2024-07-30 22:17:08 +0530

    Time: 14 ms (26.62%), Space: 10.7 MB (36.41%) - LeetHub

--- 0909-stone-game/0909-stone-game.cpp
+++ 0909-stone-game/0909-stone-game.cpp
@@ -1,6 +1,28 @@
 class Solution {
+private:
+    int dp[505][505];
 public:
-    bool stoneGame(vector<int>& piles) {
-        return true;
+
+    int fun(int lyt,int ryt,int flag,vector<int>&p){
+        if(lyt>ryt)return 0;
+        if(lyt==ryt)return p[lyt];
+        if(dp[lyt][ryt]!=-1)return dp[lyt][ryt];
+        int ans;
+        if(flag){
+             ans=max(p[lyt]+fun(lyt+1,ryt,!flag,p),p[ryt]+fun(lyt,ryt-1,!flag,p));
+        }else{
+            ans=min(-p[lyt]+fun(lyt+1,ryt,!flag,p),-p[ryt]+fun(lyt,ryt-1,!flag,p));
+
+        }
+
+        return dp[lyt][ryt]=ans;
+
+
+    }
+
+    bool stoneGame(vector<int>& p) {
+        int n=p.size();
+        memset(dp,-1,sizeof(dp));
+        return fun(0,n-1,1,p);
     }
 };
